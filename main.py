f4ac587197ef79bb307ccbeefe6695710dbc7e42
Roland Saavedra <rolandsaavedra22@gmail.com>
2026-02-28 20:34:56 -0800

    create hello and goodbye functions

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # this is some of the best code ever written.
 
+def print_hello():
+    print("Hello world")
+
+def print_goodbye():
+    print("Goodbye world")
+
 def main():
-    print("Hello world!")
+    print_hello()
+    print_goodbye()
 
 
 if __name__ == "__main__":
